225830adc9703a5e2537fc1bc22d588b7b2fcaf6
TLiuth <73227372+TLiuth@users.noreply.github.com>
2026-03-04 22:14:54 -0300

    validateAndCreateUser

--- backend-api/src/modules/users/users.service.ts
+++ backend-api/src/modules/users/users.service.ts
@@ -5,6 +5,13 @@ import { Repository } from 'typeorm';
 import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
 import { ConflictException } from '@nestjs/common';
+import { error } from 'console';
+
+type UserType = {
+    name: string,
+    email: string,
+    telephone?: string
+}
 
 @Injectable()
 export class UsersService {
@@ -26,12 +33,23 @@ export class UsersService {
             })
         
             if(existing){
-                throw ConflictException;
+                throw new ConflictException("This email is already registered");
             }
+
+            const user: UserType = {
+                name: name,
+                email: email,
+                telephone: telephone ?? ""
+            };
+
+            const entity = this.userRepository.create(user);
+
+
+            await this.userRepository.save(entity)
+
         
         }catch(error: unknown){
                 if(error instanceof ConflictException){
-                    this.logger.log(`This email is already registered`);
                     throw error;
                 }
                 this.logger.error(`Unexpected error: ${String(error)}`);
